b8d9e17e003fcd20eeef9b1c08ffaec2bf85bbc5
Michelle Gill <github@michellelynngill.com>
2016-07-03 11:08:54 -0400

    Site updated at 2016-07-03 11:08:54 -0400

--- git_push.sh
+++ git_push.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env zsh -f
 
+# Push blog to github
 cd output
 git add . -A
 git commit -m "`date '+Site updated at %Y-%m-%d %H:%M:%S %z'`"
 git push -u origin master
+cd ..
 
+# Push theme files to github
+cd themes/octopress-theme_MLG
+git add . -A
+git commit -m "`date '+Site updated at %Y-%m-%d %H:%M:%S %z'`"
+git push -u origin master
+cd ../..
+
+# Push config files to github
+git add . -A
+git commit -m "`date '+Site updated at %Y-%m-%d %H:%M:%S %z'`"
+git push -u origin master
 
