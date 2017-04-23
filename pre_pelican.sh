9ee00f1c4b5749a2a71eed62c781b21f63c934eb
Michelle Gill <michelle@michellelynngill.com>
2017-04-23 08:14:31 -0400

    Site updated at 2017-04-23 08:14:18 -0400

--- pre_pelican.sh
+++ pre_pelican.sh
@@ -5,7 +5,7 @@ if [[ -e ./output/.git ]]; then
 	find ./output -mindepth 1 | grep -v '\.git' | xargs -I{} rm -rf "{}"
 else
 	rm -rf ./output
-	git clone https://github.com/modernscientist/modernscientist.github.com.git ./output
+	git clone git@github.com:modernscientist/modernscientist.github.com.git ./output
 fi
 
 
